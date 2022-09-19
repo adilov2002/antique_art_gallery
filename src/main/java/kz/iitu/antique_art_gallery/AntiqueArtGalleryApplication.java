82a327342e813e9380bfadb7a957a855b807834c
adilov2002 <joni.2002.08@gmail.com>
2022-09-19 21:11:16 +0600

    Initial commit

---
+++ src/main/java/kz/iitu/antique_art_gallery/AntiqueArtGalleryApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.antique_art_gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiqueArtGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiqueArtGalleryApplication.class, args);
+    }
+
+}
